bdbb05659f3ae3fbcad00284e4aa752ee5a3baaf
Zain Khan <zain.khaan147@gmail.com>
2021-10-30 14:03:56 +0500

    Created root div using JS

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 export const init = (config) => {
+  const widgetDiv = document.createElement("div");
+  widgetDiv.setAttribute("id", "app-widget");
+  document.body.appendChild(widgetDiv);
+
   ReactDOM.render(
     <App config={config} />,
     document.getElementById("app-widget")
