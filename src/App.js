a81d6dfa96be0288f34d0ce2e133c69785c60363
Fawwad-Khan <fawwad.j4k@gmail.com>
2021-10-30 15:47:04 +0500

    ADD - Screenshot feature with button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import TakeABug from "./components/reportABug";
 
 export default function App({ config }) {
   return (
     <div>
       <Header title={config.title} />
+      <TakeABug />
     </div>
   );
 }
