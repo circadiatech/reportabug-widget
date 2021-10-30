ce62cf76f3bd00e2258ee680eab5a8daee0fdaf8
umernawaz0301 <umernawaz0301@gmail.com>
2021-10-30 15:24:39 +0500

    create float button with hover effect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import FixedButton from "./Components/FixedButton";
+import "./index.css";
 
 export default function App({ config }) {
   return (
     <div className="text-center">
       <Header />
       <h1>React Bug Widget</h1>
+      <FixedButton />
     </div>
   );
 }
